a662754ed5c92e494c68cd0d8e2c9d841eb009ae
Matt Sutton <mattlsutton1993@gmail.com>
2017-02-24 22:34:15 +0000

    Implemented a base class for scripts to inherit from.   + Added a filetypes list to this base class to specify which file   types to encrypt Adjusted Main.py to check process the extension of a file and check that it is in the valid list before appending it to the list of items to be encrypted

---
+++ scripts/Base.py
@@ -0,0 +1,14 @@
+## RANSOM
+## Base class
+## author mls
+
+################
+## Base class ##
+################
+class Base():
+    # Init base object
+
+    # Init class variables
+    filetypes = [
+                "jpg", "doc", "docx", "mp3", "zip" , "rar", "exe", "msi"
+                ]
