8f1257fa068fe5a2034574fea58b9bfd794437ac
Sithis <sithis@sithis.com>
2017-10-12 15:32:56 +0100

    Updated version to 2.0 in several places. Removed some TODOs and old code

--- build/ExeBuilder/__init__.py
+++ build/ExeBuilder/__init__.py
@@ -1,12 +1,12 @@
 '''
 @summary: Crypter ExeBuilder(Package): ExeBuilder with GUI for building Crypter ransomware
 @author: MLS
-@version: 0.1
+@version: 2.0
 '''
 
 # ABOUT
 __author__ = "MLS"
-__version__ = "0.1"
+__version__ = "2.0"
 __summary__ = "Provides a GUI for building the Crypter ransomware"
 
 # Import package modules
